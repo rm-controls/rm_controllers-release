22f9bf19ebb9f0abfc77634659421133060a84d3
YuuinIH <821797423@qq.com>
2022-01-08 19:33:52 +0800

    Rebase from 'upstream'

--- rm_chassis_controllers/include/rm_chassis_controllers/chassis_base.h
+++ rm_chassis_controllers/include/rm_chassis_controllers/chassis_base.h
@@ -30,13 +30,12 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
- 
+
 //
 // Created by huakang on 2021/3/21.
 //
 
-#ifndef RM_CHASSIS_CONTROLLER_CHASSIS_BASE_H_
-#define RM_CHASSIS_CONTROLLER_CHASSIS_BASE_H_
+#pragma once
 
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
@@ -49,53 +48,125 @@
 #include <geometry_msgs/Vector3Stamped.h>
 #include <nav_msgs/Odometry.h>
 
-namespace rm_chassis_controllers {
-
-struct Command {
+namespace rm_chassis_controllers
+{
+struct Command
+{
   geometry_msgs::Twist cmd_vel_;
   rm_msgs::ChassisCmd cmd_chassis_;
   ros::Time stamp_;
 };
-
-class ChassisBase : public controller_interface::MultiInterfaceController
-    <hardware_interface::EffortJointInterface, hardware_interface::RobotStateInterface> {
- public:
+template <typename... T>
+class ChassisBase : public controller_interface::MultiInterfaceController<T...>
+{
+public:
   ChassisBase() = default;
-  bool init(hardware_interface::RobotHW *robot_hw,
-            ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
-  void update(const ros::Time &time, const ros::Duration &period) override;
- protected:
+  /** @brief Get and check params for covariances. Setup odometry realtime publisher + odom message constant fields.
+   * init odom tf.
+   *
+   * @param robot_hw The robot hardware abstraction.
+   * @param root_nh A NodeHandle in the root of the controller manager namespace. This is where the ROS interfaces are
+   * setup (publishers, subscribers, services).
+   * @param controller_nh A NodeHandle in the namespace of the controller. This is where the controller-specific
+   * configuration resides.
+   * @return True if initialization was successful and the controller
+   * is ready to be started.
+   */
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
+  /** @brief Receive real_time command from manual. Execute different action according to current mode. Set
+   * necessary params of chassis. Execute power limit.
+   *
+   * Receive real_time command from manual and check whether it is normally, if can not receive command from manual
+   * for a while, chassis's velocity will be set zero to avoid out of control. Execute different action according
+   * to current mode such as RAW, FOLLOW, GYRO, TWIST.(Their specific usage will be explain in the next). UpdateOdom,Set
+   * necessary params such as Acc and vel_tfed. Execute moving action and powerlimit.
+   *
+   * @param time The current time.
+   * @param period The time passed since the last call to update.
+   */
+  void update(const ros::Time& time, const ros::Duration& period) override;
+
+protected:
+  /** @briefThe The mode RAW: original state.
+   *
+   *  The mode raw: original state. Linear velocity will be set zero to stop move.
+   */
   void raw();
-  void follow(const ros::Time &time, const ros::Duration &period);
-  void twist(const ros::Time &time, const ros::Duration &period);
+  /** @brief The mode FOLLOW: chassis will follow gimbal.
+   *
+   * The mode FOLLOW: The chassis's direct will follow gimbal's direct all the time.
+   *
+   * @param time The current time.
+   * @param period The time passed since the last call to update.
+   */
+  void follow(const ros::Time& time, const ros::Duration& period);
+  /** @brief The mode TWIST: Just moving chassis.
+   *
+   * The mode TWIST: Chassis will move independent and will not effect by gimbal's move.
+   *
+   * @param time The current time.
+   * @param period The time passed since the last call to update.
+   */
+  void twist(const ros::Time& time, const ros::Duration& period);
+  /** @brief The mode GYRO: Moving like a top.
+   *
+   * The mode GYRO: Chassis will rotate around itself.
+   */
   void gyro();
-  virtual void moveJoint(const ros::Time &time, const ros::Duration &period) = 0;
+  virtual void moveJoint(const ros::Time& time, const ros::Duration& period) = 0;
   virtual geometry_msgs::Twist forwardKinematics() = 0;
-  void updateOdom(const ros::Time &time, const ros::Duration &period);
+  /** @brief Init frame on base_link. Integral vel to pos and angle.
+   *
+   * @param time The current time.
+   * @param period The time passed since the last call to update.
+   */
+  void updateOdom(const ros::Time& time, const ros::Duration& period);
+  /** @brief Set chassis velocity to zero.
+   */
   void recovery();
-  void tfVelToBase(const std::string &from);
+  /** @brief Transform tf velocity to base link frame.
+   *
+   * @param from The father frame.
+   */
+  void tfVelToBase(const std::string& from);
+  /** @brief To limit the chassis power according to current power limit.
+   *
+   * Receive power limit from command. Set max_effort command to chassis to avoid exceed power limit.
+   */
   void powerLimit();
+  /** @brief Write current command from rm_msgs::ChassisCmd.
+   *
+   * @param msg This message contains various state parameter settings for basic chassis control
+   */
+  void cmdChassisCallback(const rm_msgs::ChassisCmdConstPtr& msg);
+  /** @brief Write current command from  geometry_msgs::Twist.
+   *
+   * @param msg This expresses velocity in free space broken into its linear and angular parts.
+   */
+  void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg);
 
-  void cmdChassisCallback(const rm_msgs::ChassisCmdConstPtr &msg);
-  void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg);
-
-  hardware_interface::EffortJointInterface *effort_joint_interface_{};
+  rm_control::RobotStateHandle robot_state_handle_{};
+  hardware_interface::EffortJointInterface* effort_joint_interface_{};
   std::vector<hardware_interface::JointHandle> joint_handles_{};
-  hardware_interface::RobotStateHandle robot_state_handle_{};
 
-  double wheel_base_{}, wheel_track_{}, wheel_radius_{}, publish_rate_{}, twist_angular_{},
-      power_coeff_{}, power_min_vel_{}, timeout_{};
+  double wheel_base_{}, wheel_track_{}, wheel_radius_{}, publish_rate_{}, twist_angular_{}, timeout_{}, effort_coeff_{},
+      velocity_coeff_{}, power_offset_{};
   bool enable_odom_tf_ = false;
   bool state_changed_ = true;
-  enum { RAW, FOLLOW, GYRO, TWIST };
+  enum
+  {
+    RAW,
+    FOLLOW,
+    GYRO,
+    TWIST
+  };
   int state_ = GYRO;
-  RampFilter<double> *ramp_x{}, *ramp_y{}, *ramp_w{};
+  RampFilter<double>*ramp_x_{}, *ramp_y_{}, *ramp_w_{};
   std::string follow_source_frame_{};
 
   ros::Time last_publish_time_;
   geometry_msgs::TransformStamped odom2base_{};
-  geometry_msgs::Vector3 vel_cmd_{}; // x, y
-  geometry_msgs::Vector3 vel_tfed_{}; // x, y
+  geometry_msgs::Vector3 vel_cmd_{};  // x, y
   control_toolbox::Pid pid_follow_;
 
   std::shared_ptr<realtime_tools::RealtimePublisher<nav_msgs::Odometry> > odom_pub_;
@@ -106,6 +177,4 @@ class ChassisBase : public controller_interface::MultiInterfaceController
   realtime_tools::RealtimeBuffer<Command> cmd_rt_buffer_;
 };
 
-}
-
-#endif // RM_CHASSIS_CONTROLLER_CHASSIS_BASE_H_
+}  // namespace rm_chassis_controllers
