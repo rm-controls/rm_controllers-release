bb94acce6cc51c2454fbddbff8105e811d6957a0
qiayuan <liaoqiayuan@gmail.com>
2022-04-14 15:24:25 +0000

    Revert "Trimmed the branch to only the rm_controllers sub directory"
    This reverts commit d59c14e02a7da040275ee05b49b73a0dc5f30890.

---
+++ rm_gimbal_controllers/include/rm_gimbal_controllers/gimbal_base.h
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Qiayuan Liao
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+//
+// Created by qiayuan on 1/16/21.
+//
+
+#pragma once
+
+#include <effort_controllers/joint_position_controller.h>
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <rm_common/hardware_interface/robot_state_interface.h>
+#include <hardware_interface/imu_sensor_interface.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <rm_msgs/GimbalCmd.h>
+#include <rm_msgs/GimbalDesError.h>
+#include <dynamic_reconfigure/server.h>
+#include <rm_gimbal_controllers/bullet_solver.h>
+
+namespace rm_gimbal_controllers
+{
+class Controller : public controller_interface::MultiInterfaceController<rm_control::RobotStateInterface,
+                                                                         hardware_interface::ImuSensorInterface,
+                                                                         hardware_interface::EffortJointInterface>
+{
+public:
+  Controller() = default;
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
+  void starting(const ros::Time& time) override;
+  void update(const ros::Time& time, const ros::Duration& period) override;
+  void setDes(const ros::Time& time, double yaw_des, double pitch_des);
+
+private:
+  void rate(const ros::Time& time, const ros::Duration& period);
+  void track(const ros::Time& time);
+  void direct(const ros::Time& time);
+  void moveJoint(const ros::Time& time, const ros::Duration& period);
+  void commandCB(const rm_msgs::GimbalCmdConstPtr& msg);
+
+  rm_control::RobotStateHandle robot_state_handle_;
+  hardware_interface::ImuSensorHandle imu_sensor_handle_;
+  effort_controllers::JointPositionController ctrl_yaw_, ctrl_pitch_;
+
+  BulletSolver* bullet_solver_{};
+
+  ros::Time last_publish_time_{};
+  std::shared_ptr<realtime_tools::RealtimePublisher<rm_msgs::GimbalDesError>> error_pub_;
+  ros::Subscriber cmd_gimbal_sub_;
+
+  realtime_tools::RealtimeBuffer<rm_msgs::GimbalCmd> cmd_rt_buffer_;
+
+  geometry_msgs::TransformStamped map2gimbal_des_, map2pitch_, map2base_;
+
+  rm_msgs::GimbalCmd cmd_gimbal_;
+  std::string gimbal_des_frame_id_{}, imu_name_{};
+
+  double publish_rate_{};
+  bool state_changed_{};
+
+  enum
+  {
+    RATE,
+    TRACK,
+    DIRECT
+  };
+  int state_ = RATE;
+};
+
+}  // namespace rm_gimbal_controllers
